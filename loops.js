96b15f10989aacc41b4bf2e04eadb3a28ec41712
Henry Welsch <welsch2@gmail.com>
2017-04-16 15:55:42 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,33 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    if (i === 1) {
+      array.push ("I am 1 strange loop.")
+    }
+    else {
+      array.push("I am ${i} strange loops.")
+    }
+  }
+  return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log("${n} more to go!");
+    n--;
+  }
+  return "done";
+}
+
+
+function MaybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array) {
+
+  do { 
+    array.pop(array.length) 
+  }
+    while (array.length > 0 && array.length>0)
+  return array;
+}
